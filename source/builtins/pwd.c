4e783d2933b9c06424d21ca68b52a4055e12827c
sarahmss <sarahmodesto@gmail.com>
2022-01-18 09:57:55 -0300

    pwd

--- source/builtins/pwd.c
+++ source/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: smodesto <smodesto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 20:31:18 by smodesto          #+#    #+#             */
-/*   Updated: 2021/12/28 00:58:16 by smodesto         ###   ########.fr       */
+/*   Updated: 2022/01/18 09:57:04 by smodesto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,10 @@
 
 int	ft_pwd(t_process *p)
 {
-	char	wd[10000];
-	int		argc;
+	char	wd[100000];
 
-	argc = 1;
-	while (p->argv[argc])
-		argc++;
-	if (argc != 1)
-		return (ft_check_error(ENUMARG, "ENV: Invalid args", NULL));
-	if (!getcwd(wd, 10000))
+	(void)p;
+	if (!getcwd(wd, 100000))
 	{
 		ft_strcpy(wd, "pwd: not able to determine current working directory");
 		return (ft_check_error(ECOMMAND, wd, NULL));
